7eb451e9ff23fc8a8d6b657f7436aeb5048465bf
Jaybert Bajenting <jaybertbajenting99@gmail.com>
2024-03-12 16:33:38 +0800

    Initial commit

---
+++ src/main/java/com/integ/focusify/FocusifyApplication.java
@@ -0,0 +1,13 @@
+package com.integ.focusify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/integ/focusify/FocusifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integ.focusify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
